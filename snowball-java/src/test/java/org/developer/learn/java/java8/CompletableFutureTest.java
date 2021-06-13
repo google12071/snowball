84850ede1fa62ce9b1d1def29a11282846c01976
lifeiqiang <Fqli12071@163.com>
2021-06-13 10:36:19 +0800

    completableFuture

--- snowball-java/src/test/java/org/developer/learn/java/java8/CompletableFutureTest.java
+++ snowball-java/src/test/java/org/developer/learn/java/java8/CompletableFutureTest.java
@@ -14,6 +14,6 @@ public class CompletableFutureTest {
         CompletableFuture<Integer> future = new CompletableFuture<>();
         new Thread(new AskThread(future)).start();
         Thread.sleep(5000);
-        future.complete(5);
+        future.complete(60);
     }
 }
