644827fd7a5b6612478c84b2ca0d470340924aca
lifeiqiang <Fqli12071@163.com>
2021-06-13 10:48:56 +0800

    completableFuture

--- snowball-java/src/test/java/org/developer/learn/java/java8/CompletableFutureTest.java
+++ snowball-java/src/test/java/org/developer/learn/java/java8/CompletableFutureTest.java
@@ -5,15 +5,35 @@ import org.developer.learn.java.java8.completableFuture.AskThread;
 import org.junit.Test;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 @Slf4j
 public class CompletableFutureTest {
 
     @Test
-    public void asyncCalculate() throws InterruptedException {
-        CompletableFuture<Integer> future = new CompletableFuture<>();
-        new Thread(new AskThread(future)).start();
-        Thread.sleep(5000);
-        future.complete(60);
+    public void asyncCalculate() throws InterruptedException, ExecutionException, TimeoutException {
+//        CompletableFuture<Integer> future = new CompletableFuture<>();
+//        new Thread(new AskThread(future)).start();
+//        Thread.sleep(5000);
+//        future.complete(60);
+
+        final CompletableFuture<Integer> future2 = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(6000);
+                return 30 / 0;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return 0;
+        }).exceptionally(e -> {
+            log.error("calculate failure error", e);
+            return 0;
+        });
+
+        Integer res = future2.get(  1, TimeUnit.SECONDS);
+        System.out.println(res);
     }
+
 }
