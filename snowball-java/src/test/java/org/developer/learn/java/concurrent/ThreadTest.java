4de3d8535c54ee9920bc7972981dc933498764ea
lifeiqiang <Fqli12071@163.com>
2021-07-05 08:13:02 +0800

    线程基本操作

--- snowball-java/src/test/java/org/developer/learn/java/concurrent/ThreadTest.java
+++ snowball-java/src/test/java/org/developer/learn/java/concurrent/ThreadTest.java
@@ -24,7 +24,7 @@ public class ThreadTest {
      * 自定义线程池
      */
     @Test
-    public void customThread() throws InterruptedException {
+    public void customThread() {
         ExecutorService executor = new MyThreadPoolExecutor(5, 5, 10L
                 , TimeUnit.MILLISECONDS, new LinkedBlockingDeque<>(10), r -> {
             Thread t = new Thread(r);
@@ -57,11 +57,47 @@ public class ThreadTest {
     }
 
     @Test
-    public void threadPool()throws Exception{
+    public void threadPool() throws Exception {
         ExecutorService executorService = Executors.newFixedThreadPool(10);
         for (int i = 0; i < 10; i++) {
             executorService.execute(new MyThread());
         }
         Thread.sleep(2000);
+    }
+
+
+    /**
+     * 线程中断并设置中断标识位
+     */
+    @Test
+    public void threadInterrupt() {
+        log.info("currentThread:{}", Thread.currentThread().getName());
+        Thread t1 = new Thread(() -> {
+            while (true) {
+                //当前线程被中断，则直接退出
+                if (Thread.currentThread().isInterrupted()) {
+                    log.info("ThreadInterrupted:{}", Thread.currentThread().getName());
+                    break;
+                }
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    log.info("Interrupted when sleep,Thread:{}", Thread.currentThread().getName());
+                    //当前线程设置中断标记
+                    Thread.currentThread().interrupt();
+                    e.printStackTrace();
+                }
+            }
+        }, "thread1");
+        t1.start();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            log.info("Interrupted when sleep,Thread:{}", Thread.currentThread().getName());
+        }
+        //中断线程1
+        t1.interrupt();
     }
 }
